d46d8a9b94186cbddc95158b5dcd520e84269804
Waithera Kamau <cynthiawaithera@gmail.com>
2023-02-23 01:16:19 -0800

    4.isalpha

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 
 int main(void)
@@ -18,6 +18,8 @@ int main(void)
 	_putchar(r + '0');
 	r = _isalpha(';');
 	_putchar(r + '0');
+
 	_putchar('\n');
+
 	return (0);
 }
